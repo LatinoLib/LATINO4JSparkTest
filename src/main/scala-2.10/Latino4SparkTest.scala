5c2b7f631191acb1f2a83880380c48f80216519e
mgrcar <miha.grcar@ijs.si>
2016-02-18 15:50:55 +0100

    Code cleanup

--- src/main/scala-2.10/Latino4SparkTest.scala
+++ src/main/scala-2.10/Latino4SparkTest.scala
@@ -3,16 +3,16 @@ import org.apache.spark.SparkConf
 
 import org.latinolib.Language
 
-object Latino4SparkTest{
+object Latino4SparkTest {
   def main(args: Array[String]) {
     System.setProperty("hadoop.home.dir", "c:\\hadoop\\")
 
-    val logFile = "C:\\spark\\README.md"
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val conf = new SparkConf().setAppName("LATINO4J Hello World").setMaster("local")
     val sc = new SparkContext(conf)
+
+    val logFile = "C:\\spark\\README.md"
+
     val logData = sc.textFile(logFile, 2).cache()
-//    val numAs = logData.filter(line => line.contains("a")).count()
-//    val numBs = logData.filter(line => line.contains("b")).count()
     val lang = logData
       .map(line => Language.detect(line))
       .groupBy(lang => lang)
@@ -21,7 +21,6 @@ object Latino4SparkTest{
     val langLocal = lang.collect()
 
     println("----------------------------------------------------------")
-//    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
     langLocal.foreach(item => println(item))
     println("----------------------------------------------------------")
   }
